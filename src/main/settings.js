d14a0ce8f813ca837554f41c0dcb0cac0a42bfa5
jc <jpappachan>
2017-11-08 20:55:17 +0530

    remove settings

--- src/main/settings.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  NAME : 'Quick Jira',
-  JIRA_BASE_URL : 'https:///jira/',
-  GET_RESULTS_API : 'rest/api/2/search?fields=*navigable,comment&jql=',
-  JIRA_USER : '',
-  JIRA_PASSWORD : ''
-}
-
-module.exports = config;
